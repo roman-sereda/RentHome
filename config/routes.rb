957aac35e5750defe52dbf73b981a5c6f8847ea8
Rems Palyvoda <rems.palyvoda@gmail.com>
2017-05-03 16:12:10 +0300

    add devise-token-auth, generate Host and Guest users

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'Host', at: 'host_auth'
+
+  mount_devise_token_auth_for 'Guest', at: 'guest_auth'
+  as :guest do
+    # Define routes for Guest within this block.
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
