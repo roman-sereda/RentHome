c41627d6982040e202c9c446a625d65901c9bfc8
roman-sereda <romansereda98@gmail.com>
2017-05-04 14:21:35 +0300

    Add Card

--- frontend/src/components/Welcome/index.js
+++ frontend/src/components/Welcome/index.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react'
 
 import Footer from './../Footer'
 import Header from './../Header'
+import Card from './../Card'
 
 export default class WelcomePage extends Component{
   render(){
     return(
       <span>
         <Header />
+        <Card />
         <Footer />
       </span>
     )
