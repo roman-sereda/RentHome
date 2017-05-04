61fb05ec6086cceccfcca7422c9b80c0e5f1fbbf
roman-sereda <romansereda98@gmail.com>
2017-05-04 12:15:18 +0300

    Add empty Welcome and Header views

---
+++ frontend/src/components/Welcome/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+import Footer from './../Footer'
+import Header from './../Header'
+
+export default class WelcomePage extends Component{
+  render(){
+    return(
+      <span>
+        <Header />
+        <Footer />
+      </span>
+    )
+  }
+}
