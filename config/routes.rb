e868a852691a69e2b9333ec9a36ed91840d31846
Roman Mazurets <sg.romanmazurets@gmail.com>
2017-05-04 18:01:06 +0300

    fix routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,9 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'Host', at: 'host_auth'
 
   mount_devise_token_auth_for 'Guest', at: 'guest_auth'
-  as :guest do
-    # Define routes for Guest within this block.
-  end
-
-  resource :houses, only: [:show, :create, :update, :destroy]
+  
+  resources :guests, only: [:show, :create, :update, :destroy]
+  resources :houses, only: [:show, :create, :update, :destroy]
   get "/houses/search", to: "houses#search"
 
 end
