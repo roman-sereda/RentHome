d8ef3f910b540633d6f5b286c0efa4b293345bbf
roman-sereda <romansereda98@gmail.com>
2017-05-05 13:12:37 +0300

    refactor Card component, end some content to welcome page

--- frontend/src/components/Button/index.js
+++ frontend/src/components/Button/index.js
@@ -3,8 +3,9 @@ import styles from './styles.css'
 
 export default class Button extends Component{
   render(){
+    const propsToStyle = this.props.white ? { opacity: 0.8 } : null
     return(
-      <div className = 'button'>
+      <div className = 'button' style = { propsToStyle } >
         { this.props.label }
       </div>
     )
--- frontend/src/components/Card/index.js
+++ frontend/src/components/Card/index.js
@@ -4,10 +4,11 @@ import styles from './styles.css'
 import Button from '../Button'
 
 export default class Card extends Component{
-  render(){
+
+  imageWithTextCard(){
     return(
-      <span>
-        <div className = 'card'>
+      <div className = 'card'>
+        <div className = 'img-txt'>
           <div className = 'description black-text'>Трихкімнатна квартира в центрі Рівного</div>
           <div className = 'town'>м.Рівне</div>
           <div className = 'card-image-wrapper'>
@@ -17,8 +18,42 @@ export default class Card extends Component{
             <span className = 'price-text'>899</span>
             <span className = 'price-text secondary'>грн/міс</span>
           </div>
-          <Button label = 'Перегляд' />
+          <Button label = 'Перегляд' className = 'card-button' />
         </div>
+      </div>
+    )
+  }
+
+  imageCard(){
+    return(
+      <div className = 'card img' >
+        <div className = 'img-card-image-wrapper' style = {{ backgroundImage: "url('https://static1.comicvine.com/uploads/original/11128/111283068/5260519-franku+%28kys%29.jpg')"}}></div>
+      </div>
+    )
+  }
+
+  textCard(){
+    return(
+      <div className = 'card txt'>
+        <div className = 'txt-title black-text'>Як здати житло в оренду?</div>
+        <div className = 'txt-text black-text'>
+          Есть много вариантов Lorem Ipsum, но большинство из них имеет не всегда
+          приемлимые модификации, например, юмористические вставки или
+        </div>
+        <Button label = 'Здати Житло' className = 'txt-button' />
+      </div>
+    )
+  }
+
+  render(){
+    const card = this.props.card_type == 'img-txt' ?
+                    this.imageWithTextCard() :
+                      this.props.card_type == 'img' ?
+                        this.imageCard() :
+                        this.textCard()
+    return(
+      <span>
+        { card }
       </span>
     )
   }
