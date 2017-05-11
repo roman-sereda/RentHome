1828423996b3e0af620df8e35bf1366044f74e3d
roman-sereda <romansereda98@gmail.com>
2017-05-11 09:42:22 +0300

    Style SignIn page

--- frontend/src/components/Auth/SignInForm.js
+++ frontend/src/components/Auth/SignInForm.js
@@ -3,12 +3,39 @@ import React, { Component } from 'react'
 import Button from '../Button'
 
 export default class SignIn extends Component {
+  constructor(props){
+    super(props)
+    this.state = { role: null}
+  }
+
+  chooseRole(role){
+    this.setState({ role: role })
+  }
 
   render(){
     return(
-      <span>
-        hyi
-      </span>
+      <div>
+        <div className = 'auth-blocks'>
+          <div  className = 'auth-block-wrapper' >
+            <div className = 'auth-block blue-text'>
+              <img src = '/loop.png' />
+              <div className = 'auth-button-label' >Я шукаю житло</div>
+            </div>
+          </div>
+          <div  className = 'auth-block-wrapper' >
+            <div className = 'auth-block blue-text'>
+              <img src = '/home.png' />
+              <div className = 'auth-button-label' >Я здаю житло</div>
+            </div>
+          </div>
+        </div>
+        <div className = 'auth-social-button'><Button label = 'Увійти через Facebook' link = '/#' /></div>
+        <div className = 'auth-social-button'><Button label = 'Увійти через Goole+' link = '/#' /></div>
+        <div className = 'black-text auth-separator-text'> Або </div>
+        <div className = 'auth-input' ><input placeholder = 'Email' className = 'email-input input' /></div>
+        <div className = 'auth-input' ><input placeholder = 'Пароль' className = 'pass-input input' /></div>
+        <div className = 'auth-social-button'><Button label = 'Зареєструватися' link = '/#' /></div>
+      </div>
     )
   }
 }
--- frontend/src/router.js
+++ frontend/src/router.js
@@ -5,15 +5,15 @@ import { Router, Route } from 'react-router'
 
 import WelcomePage      from './components/Welcome'
 import Auth             from './components/Auth'
-import SignUp           from './components/Auth/SignInForm'
-import SignIn           from './components/Auth/SignUpForm'
+import SignUp           from './components/Auth/SignUpForm'
+import SignIn           from './components/Auth/SignInForm'
 
 export default (
   <Router history = { browserHistory} >
     <Route path="/" component={ WelcomePage } >
       <Route path="/auth" component={ Auth } >
-        <Route path = '/auth/signin' component={ SignIn } />
-        <Route path = '/auth/signup' component={ SignUp } />
+        <Route path = '/auth/signin' component={ SignIn } auth = 'signin' />
+        <Route path = '/auth/signup' component={ SignUp } auth = 'signup' />
       </Route>
     </Route>
   </Router>
