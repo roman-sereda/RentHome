0972081709b67432b0187ed6568aa8bd6d3807b5
roman-sereda <romansereda98@gmail.com>
2017-05-11 10:21:22 +0300

    Add animation to chosen role

--- frontend/src/components/Auth/SignInForm.js
+++ frontend/src/components/Auth/SignInForm.js
@@ -3,32 +3,11 @@ import React, { Component } from 'react'
 import Button from '../Button'
 
 export default class SignIn extends Component {
-  constructor(props){
-    super(props)
-    this.state = { role: null}
-  }
-
-  chooseRole(role){
-    this.setState({ role: role })
-  }
 
   render(){
     return(
       <div>
-        <div className = 'auth-blocks'>
-          <div  className = 'auth-block-wrapper' >
-            <div className = 'auth-block blue-text'>
-              <img src = '/loop.png' />
-              <div className = 'auth-button-label' >Я шукаю житло</div>
-            </div>
-          </div>
-          <div  className = 'auth-block-wrapper' >
-            <div className = 'auth-block blue-text'>
-              <img src = '/home.png' />
-              <div className = 'auth-button-label' >Я здаю житло</div>
-            </div>
-          </div>
-        </div>
+        <div className = 'space-between-auth-buttons' />
         <div className = 'auth-social-button'><Button label = 'Увійти через Facebook' link = '/#' /></div>
         <div className = 'auth-social-button'><Button label = 'Увійти через Goole+' link = '/#' /></div>
         <div className = 'black-text auth-separator-text'> Або </div>
