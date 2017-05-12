0ec2eee95e19bd91b218915edc4ed591bff006e9
Andrew Pelykh <andrew.pelykh@gmail.com>
2017-05-12 09:50:04 +0300

    Refactor houses controller tests

--- spec/controllers/houses_controller_spec.rb
+++ spec/controllers/houses_controller_spec.rb
@@ -3,70 +3,66 @@ require "rails_helper"
 RSpec.describe HousesController, type: :controller do
   include Devise::Test::ControllerHelpers
 
+  let(:host) { create(:host) }
+  let(:wrong_house) { create(:house) }
+  let(:proper_house) { create(:house, host: host) }
+  let(:house_count) { House.count }
+
   describe "GET #show" do
-    before(:each) do
-
-      @house = create(:house)
-      expect(@house.impressionist_count(:filter=>:ip_address)).to eql 0
-      get :show, params: { id: @house.id }
+    before do
+      expect(proper_house.impressionist_count(:filter=>:ip_address)).to eql 0
+      get :show, params: { id: proper_house.id }
     end
 
-    it "should return house data" do
-      expect(json_response[:house][:city]).to eql @house.city
+    it "returns house data" do
+      expect(json_response[:house][:city]).to eql proper_house.city
     end
 
-    it "should count unique ip" do
-      expect(@house.impressionist_count(:filter=>:ip_address)).to eql 1
+    it "counts unique ip" do
+      expect(proper_house.impressionist_count(:filter=>:ip_address)).to eql 1
     end
 
     it { should respond_with 200 }
-
   end
 
   describe "POST #create" do
-
     context "when host is authentificated" do
-
-      before(:each) do
-        sign_in create(:host), scope: :host
+      before do
+        sign_in host, scope: :host
       end
 
       context "when is created successfully" do
-        before(:each) do
-          @house_count = House.count
+        before do
+          house_count
           post :create, params: { house: attributes_for(:house) }
         end
 
         it { should respond_with 201 }
 
-        it "should create new house" do
-          expect(House.count).to eql @house_count + 1
+        it "creates new house" do
+          expect(House.count).to eql house_count + 1
         end
 
         it "should return new house" do
           expect(json_response[:house][:city]).to eql House.last.city
         end
-
       end
 
       context "when is not created" do
-
-        before(:each) do
+        before do
           post :create, params: { house: attributes_for(:house, city: nil) }
         end
 
         it { should respond_with 422 }
 
-        it "should return errors" do
+        it "returns errors" do
           expect(json_response).to have_key(:errors)
         end
-
       end
     end
 
     context "when host is not authentificated" do
-
-      before(:each) do
+      before do
         post :create, params: { house: attributes_for(:house) }
       end
 
@@ -75,105 +71,101 @@ RSpec.describe HousesController, type: :controller do
   end
 
   describe "PATCH #update" do
-
     context "when host is authentificated" do
-      before(:each) do
-        @host = create(:host)
-        @house = create(:house, host: @host)
-        sign_in @host, scope: :host
+      before do
+        sign_in host, scope: :host
       end
 
-      context "and he updates one of his houses" do
-
+      context "when updating proper house" do
         context "when is updated successfully" do
-          before(:each) do
-            patch :update, params: { id: @house.id,
+          before do
+            patch :update, params: { id: proper_house.id,
               house: attributes_for(:house, city: "New city") }
           end
 
           it { should respond_with 200 }
 
-          it "should return updated house" do
-            expect(json_response[:house][:id]).to eql @house.id
+          it "returns updated house" do
+            expect(json_response[:house][:id]).to eql proper_house.id
           end
         end
 
         context "when is not updated" do
-          before(:each) do
-            patch :update, params: { id: @house.id,
+          before do
+            patch :update, params: { id: proper_house.id,
               house: attributes_for(:house, city: nil) }
           end
 
           it { should respond_with 422 }
 
-          it "should return errors" do
+          it "returns errors" do
             expect(json_response).to have_key(:errors)
           end
         end
       end
 
-      context "and he updates house of other host" do
-
-        it do
+      context "when try to update wront house" do
+        it "returns authentication errors" do
           patch :update, params: { id: create(:house).id,
             house: attributes_for(:house, city: nil)}
           should respond_with 401
         end
       end
-
     end
   end
 
   describe "#DELETE #destroy" do
+    before do
+      sign_in host, scope: :host
+    end
 
     context "when associated host is authentificated" do
-
-      before(:each) do
-        host = create(:host)
-        sign_in host, scope: :host
-        @house = create(:house, host: host)
-        @house_count = House.count
-        delete :destroy, params: { id: @house.id }
+      before do
+        proper_house
+        house_count
+        delete :destroy, params: { id: proper_house.id }
+        house_count
       end
 
       it { should respond_with 204 }
 
-      it "should delete house" do
-        expect(House.count).to eql @house_count - 1
+      it "deletes house" do
+        expect(House.count).to eql house_count - 1
       end
     end
 
-    it "when associated host is authentificated" do
-      sign_in create(:host), scope: :host
-      @house = create(:house)
-      delete :destroy, params: { id: @house.id }
-      should respond_with 401
+    context "when wrong host is authentificated" do
+      before do
+        delete :destroy, params: { id: wrong_house.id }
+      end
+
+      it { should respond_with 401 }
     end
   end
 
   describe "GET #search" do
+    let(:houses) do
+      create(:house)
+      create(:house, city: "new city")
+      create(:house, city: "new city", wi_fi: false)
+    end
 
     context "when filter is provided" do
-      before(:each) do
-        create(:house)
-        create(:house, city: "new city")
-        create(:house, city: "new city", wi_fi: false)
-        get :search, params: { page: 1, filters: {
-                                          city: "new city",
-                                          wi_fi: false
-                                        }}
+      before do
+        houses
+        get :search, params: { page: 1,
+          filters: { city: "new city", wi_fi: false }}
       end
 
       it { should respond_with 200 }
 
-      it 'should return array of filtered houses' do
+      it 'returns array of filtered houses' do
         expect(json_response[:houses].count).to eql 1
       end
-
     end
 
     context "when filter is not provided" do
-      before(:each) do
+      before do
         create(:house)
         create(:house, city: "new city")
         create(:house, city: "new city", wi_fi: false)
@@ -182,7 +174,7 @@ RSpec.describe HousesController, type: :controller do
 
       it { should respond_with 200 }
 
-      it 'should return array of filtered houses' do
+      it 'returns array of filtered houses' do
         expect(json_response[:houses].count).to eql 3
       end
     end
