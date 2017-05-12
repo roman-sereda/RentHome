c52f97b42abcb4083c7ea068d85dd309c9698374
VasiliyDybinchyc <VasiliyDybinchyk@gmail.com>
2017-05-12 17:59:39 +0300

    add j-toker request

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,6 @@ module Home
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.api_only = true
+    config.api_only = false
   end
 end
