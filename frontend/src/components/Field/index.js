c67f83ed3c2d909869b3c61f40df0cbc1b10776e
roman-sereda <romansereda98@gmail.com>
2017-05-13 15:54:49 +0300

    Fix useless space

--- frontend/src/components/Field/index.js
+++ frontend/src/components/Field/index.js
@@ -7,7 +7,7 @@ import Button from '../Button'
 import Calendar from '../Calendar'
 
 const search_bar_conf = {
-  lg:8,  md: 10, sm: 12, xs: 12
+  lg:8,  md: 10, sm: 10, xs: 10
 }
 
 const input_conf = {
@@ -16,11 +16,18 @@ const input_conf = {
 
 export default class Field extends Component{
 
+  isMobile(){
+    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+       return true
+    }
+    return false
+  }
+
   render(){
     return(
       <Row>
-        <Col lg = {2} md = {1} />
-        <Col {...search_bar_conf} className = 'search-bar'>
+        <Col lg = {2} md = {1} sm = {1} xs = {1} />
+        <Col {...search_bar_conf} className = { this.isMobile() ? "search-bar-mob" : "search-bar" }>
           <div>
             <input {...input_conf} className = 'input' />
             <span className = 'search-calendar'><Calendar type='Arrival' /></span>
@@ -28,6 +35,7 @@ export default class Field extends Component{
             <Button label = 'Шукати' />
           </div>
         </Col>
+        <Col lg = {2} md = {1} sm = {1} xs = {1} />
       </Row>
     )
   }
