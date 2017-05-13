3be5c60f22dad083c17a1d89e959959a32767e64
Roman Mazurets <sg.romanmazurets@gmail.com>
2017-05-13 14:00:40 +0300

    fix g+ registration

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module Home
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.api_only = false
+    config.api_only = true
+    config.session_store :cookie_store, key: '_interslice_session'
+    config.middleware.use ActionDispatch::Cookies # Required for all session management
+    config.middleware.use ActionDispatch::Session::CookieStore, config.session_options
   end
 end
