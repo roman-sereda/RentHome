12d9263a6ac86828513ec52330ff790cc95d80b9
roman-sereda <romansereda98@gmail.com>
2017-05-16 14:15:54 +0300

    Fix header style

--- frontend/src/components/Header/MainHeader.js
+++ frontend/src/components/Header/MainHeader.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import styles from './styles.css'
+import { Grid, Row, Col } from 'react-flexbox-grid'
 
 import Button from '../Button'
 
@@ -7,23 +8,25 @@ export default class MainHeader extends Component {
   render(){
     return(
       <div className = 'header full-width'>
-        <span className = 'header-celll'>
-          <img src = 'header-logo.png' className = 'header-image' />
-        </span>
-        <span className = 'header-links-wrapper header-cell' >
-          { this.props.links.map((link, index) => {
-            return(
-              <span className = 'blue-text' key = { link[0]} >
-                { index == 0 ? null : <span className = 'separator' >|</span>}
-                <span>{ link[0] }</span>
-              </span>
-            )
-          })}
-        </span>
-        <span className = 'header-auth-buttons header-cell'>
-          <span className = 'header-auth-button'><Button link = '/auth/signup' label = 'Зареєструватися' /></span>
-          <span className = 'header-auth-button'><Button link = '/auth/signin' label = 'Увійти' /></span>
-        </span>
+        <Col lgOffset = { 2 } lg = { 8 }>
+          <span className = 'header-cell'>
+            <img src = 'header-logo.png' className = 'header-image' />
+          </span>
+          <span className = 'header-links-wrapper header-cell' >
+            { this.props.links.map((link, index) => {
+              return(
+                <span className = 'blue-text' key = { link[0]} >
+                  { index == 0 ? null : <span className = 'separator' >|</span>}
+                  <span>{ link[0] }</span>
+                </span>
+              )
+            })}
+          </span>
+          <span className = 'header-auth-buttons header-cell'>
+            <span className = 'header-auth-button'><Button link = '/auth/signup' label = 'Зареєструватися' /></span>
+            <span className = 'header-auth-button'><Button link = '/auth/signin' label = 'Увійти' /></span>
+          </span>
+        </Col>
       </div>
     )
   }
