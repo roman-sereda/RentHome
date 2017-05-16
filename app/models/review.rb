9947a5130930b13068e3f4fde1e79ee0699e4869
Andrew Pelykh <andrew.pelykh@gmail.com>
2017-05-16 10:30:29 +0300

    Add create and index reviews actions

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,5 @@
 class Review < ApplicationRecord
+
   belongs_to :guest
   belongs_to :house
 
@@ -6,10 +7,20 @@ class Review < ApplicationRecord
   validates :rating, numericality: { greater_than_or_equal_to: 0,
                                     less_than_or_equal_to: 10 }
   validates :text, length: { maximum: 200 }
+  validate :if_guest_visited_house
 
-  before_save do
-    order = Order.find_by(house_id: house_id, guest_id: guest_id)
-    errors.add(:guest,
-      "You should to visit this house to be able to review it") and throw :abort unless order
-  end
+  after_save :update_house_rating
+
+  private
+
+    def update_house_rating
+      count = house.reviews.count
+      house.update_attributes(rating: (((count-1) * house.rating + rating)/count).round)
+    end
+
+    def if_guest_visited_house
+      order = Order.where(house_id: house_id, guest_id: guest_id)
+      errors.add(:guest,
+        "You can write 1 review per 1 visit") unless house.reviews.count < order.count
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170515103545) do
+ActiveRecord::Schema.define(version: 20170515143204) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -95,12 +95,13 @@ ActiveRecord::Schema.define(version: 20170515103545) do
     t.date     "rent_end"
     t.integer  "rooms"
     t.string   "city"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
     t.decimal  "price_per_day"
     t.decimal  "price_per_month"
     t.integer  "host_id"
     t.text     "description"
+    t.integer  "rating",          default: 0
     t.index ["host_id"], name: "index_houses_on_host_id", using: :btree
   end
 
