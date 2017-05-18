0c1ea3326053ffb9658256236002b2b32d56e928
roman-sereda <romansereda98@gmail.com>
2017-05-18 11:28:42 +0300

    Add choose role to separate component

--- frontend/src/components/Auth/SignUpForm.js
+++ frontend/src/components/Auth/SignUpForm.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 
 import Button from '../Button'
+import Role from './Role'
 
 import { createUser,
          createSessionWithFacebook,
@@ -12,20 +13,11 @@ export default class SignUp extends Component {
     this.state = { role: null}
   }
 
-  chooseRole(role){
+  changeRole(role){
     this.setState({ role: role })
   }
 
-  addEffectToChosenRole(role){
-    console.log(this)
-    document.getElementById('search').className = 'auth-block blue-text'
-    document.getElementById('rent').className = 'auth-block blue-text'
-    document.getElementById(role).className = 'auth-block blue-text chosen-role'
-  }
-
   registrationEmail(){
-
-    let dataUser = {}
 
     console.log(this.refs['email-input'].value)
 
@@ -38,20 +30,7 @@ export default class SignUp extends Component {
   render(){
     return(
       <div>
-        <div className = 'auth-blocks'>
-          <div  className = 'auth-block-wrapper' >
-            <div onClick = { () => this.addEffectToChosenRole('search')} id = 'search' className = 'auth-block blue-text'>
-              <img src = '/loop.png' />
-              <div className = 'auth-button-label' >Я шукаю житло</div>
-            </div>
-          </div>
-          <div className = 'auth-block-wrapper' >
-            <div onClick = { () => this.addEffectToChosenRole('rent')} id = 'rent' className = 'auth-block blue-text'>
-              <img src = '/home.png' />
-              <div className = 'auth-button-label' >Я здаю житло</div>
-            </div>
-          </div>
-        </div>
+        <Role changeRole = {( role ) => this.changeRole( role )} />
         <div className = 'auth-social-button'><Button label = 'Увійти через Facebook' handleClick = {() => createSessionWithFacebook() } /></div>
         <div className = 'auth-social-button'><Button label = 'Увійти через Google+' handleClick = {() => createUserWithGoogle() } /></div>
         <div className = 'black-text auth-separator-text'> Або </div>
